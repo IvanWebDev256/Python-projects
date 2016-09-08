c77d8ea5ee1cc03e85ccaa1671283c15a694243c
IvanWebDev256 <john.parker256@mail.ru>
2016-09-08 21:46:47 +0300

    Revert "significant changes"
    This reverts commit 3a78e1ba7b2b40d5e18ae40168e644518bfcab47.
    
    # Conflicts:
    #	shopping/.idea/workspace.xml

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -1,12 +1,47 @@
-import sales.shopping_cart, sales.shopping_order
 
-cart = sales.shopping_cart.Cart()
-order = sales.shopping_order.Order()
-order.get_input()
 
-while not order.quit:
-    cart.process(order)
-    order = sales.shopping_order.Order()
-    order.get_input()
+def get_order():
+    print("[command] [item] (command is a to add, d to delete, q to quit")
+    line = input()
+    command = line[:1]
+    item = line[2:]
+    return command, item
 
-print(cart)
+
+def add_to_cart(item, cart):
+    if item not in cart:
+        cart[item] = 0
+    cart[item] += 1
+
+
+def remove_from_cart(item, cart):
+    if item in cart:
+        cart[item] -= 1
+    if cart[item] == 0:
+        cart.pop(item)
+
+
+def process_order(order, cart):
+    command, item = order
+    if command == "a":
+        add_to_cart(item, cart)
+    elif command == "d" and item in cart:
+        remove_from_cart(item, cart)
+    elif command == "q":
+        return False
+
+    return True
+
+
+def go_shopping():
+    cart = dict()
+    
+    while True:
+        order = get_order()
+        if not process_order(order, cart):
+            break
+
+    print(cart)
+    print("Finish shopping")
+
+go_shopping()
